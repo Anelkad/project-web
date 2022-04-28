70a7009dbcbdabb9687dfaed1c6119527d0308f2
dmasakbay <dmasakbay@gmail.com>
2022-04-28 22:26:01 +0600

    ehh

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AdditionComponent } from './addition/addition.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'cart', component: CartComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'categories/:categoryID/products', component: ProductsComponent},
+  { path: 'categories/:categoryID/products/:id', component: ProductDetailComponent},
+  { path: 'products/:id/addition', component: AdditionComponent},
+  { path: '',   redirectTo: '/register', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AdditionComponent } from './addition/addition.component';
+import { CartComponent } from './cart/cart.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutComponent,
+    CategoriesComponent,
+    ProductDetailComponent,
+    AdditionComponent,
+    CartComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
